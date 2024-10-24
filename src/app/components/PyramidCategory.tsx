26e6053440b4185cf0a3e2c1f01f7065647c2528
Juan Pablo Guevara López <juanpabloguevara8b@gmail.com>
2024-10-24 13:00:47 -0500

    ajusta tipado

--- src/app/components/PyramidCategory.tsx
+++ src/app/components/PyramidCategory.tsx
@@ -3,8 +3,13 @@
 import React from 'react';
 import styles from "@/app/gallery/Gallery.module.scss";
 
-export default function PyramidCategory({ category, niches }) {
-  const columns = [];
+interface PyramidCategoryProps {
+  category: string;
+  niches: string[];
+}
+
+export default function PyramidCategory({ category, niches }: PyramidCategoryProps) {
+  const columns: string[][] = [];
   let index = 0;
   let columnLength = niches.length;
 
@@ -21,8 +26,8 @@ export default function PyramidCategory({ category, niches }) {
       <div className={styles.pyramidContainerHorizontal}>
         {columns.map((column, columnIndex) => (
           <div key={columnIndex} className={`${styles.pyramidColumn} ${styles[`columnLevel${columnIndex}`]}`}>
-            {column.map((niche, nicheIndex) => (
-              <a href="#" className={styles.nicheItem}>{niche}</a>
+            {column.map((niche: string, nicheIndex: number) => (
+              <a key={nicheIndex} href="#" className={styles.nicheItem}>{niche}</a>
             ))}
           </div>
         ))}

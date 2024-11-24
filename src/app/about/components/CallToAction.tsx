7ec4e591d9967a6044303d9a59248d5707cbd583
Sebastian Preciado <preciadoproducer@gmail.com>
2024-11-24 02:32:39 -0500

    fix: add animations to CallToAction component and enhance button styles

--- src/app/about/components/CallToAction.tsx
+++ src/app/about/components/CallToAction.tsx
@@ -1,84 +1,78 @@
-// src/app/components/CallToAction.tsx
+"use client";
 
-import { Button, Flex, Icon, Text } from '@/once-ui/components';
-import styles from '@/app/about/about.module.scss';
-import Link from './Link';
-import clsx from 'clsx';
+import { Button, Flex, Icon } from "@/once-ui/components";
+import styles from "@/app/about/about.module.scss";
+import Link from "./Link";
+import clsx from "clsx";
 
-const CustomHeading: React.FC = () => {
-    return (
-        <div className={styles.customHeading}>
-            <h1 className={styles.primaryHeading}>Let’s Work</h1>
-            <h2 className={styles.secondaryHeading}>Together!</h2>
-        </div>
-    );
-};
+const CustomHeading: React.FC = () => (
+    <div className={styles.customHeading}>
+        <h1 className={styles.primaryHeading}>Let’s Work</h1>
+        <h2 className={styles.secondaryHeading}>Together!</h2>
+    </div>
+);
 
-const CustomText: React.FC = () => {
-    return (
-        <p className={styles.text}>
-            It’s time to make<br />
-            something extraordinary <br />
-            that leaves a lasting impression.
-        </p>
-    );
-};
+const CustomText: React.FC = () => (
+    <p className={styles.text}>
+        It’s time to make<br />
+        something extraordinary <br />
+        that leaves a lasting impression.
+    </p>
+);
 
+const CallToAction: React.FC = () => (
+    <Flex
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        className={styles.callToAction}
+    >
+        {/* Ícono decorativo */}
+        <Icon
+            name="handshake"
+            size="xl"
+            onBackground="accent-strong"
+            decorative
+            aria-hidden="true"
+            className={styles.icon}
+        />
 
-const CallToAction: React.FC = () => {
-    return (
-        <Flex
-            direction="column"
-            alignItems="center"
-            justifyContent="center"
-            className={styles.callToAction}
+        {/* Encabezado personalizado */}
+        <CustomHeading />
+
+        {/* Texto descriptivo */}
+        <CustomText />
+
+        {/* Botón principal */}
+        <Button
+            href="/contact"
+            variant="primary"
+            size="m"
+            className={styles.button}
         >
-            <Icon
-                name="handshake"
-                size="xl"
-                onBackground="accent-strong"
-                decorative
-                aria-hidden="true"
-                className={styles.icon}
-            />
+            Contact Now
+        </Button>
 
-            {/* Título personalizado */}
-            <CustomHeading />
-
-            {/* Descripción */}
-            <CustomText />
-
-            {/* Botón principal */}
-            <Button
-                href="/contact"
-                variant="primary"
-                size="m"
-                className={styles.button}
+        {/* Enlaces adicionales */}
+        <Flex
+            className={styles.additionalLinks}
+            gap="8"
+            marginTop="m"
+        >
+            <Link
+                href="/faqs"
+                className={clsx(styles.faqLink)}
             >
-                Contact Now
-            </Button>
-
-            {/* Enlace adicional como texto de bajo contraste */}
-            <Flex
-                className={styles.additionalLinks}
-                gap="8"
-                marginTop="m"
+                Don’t forget to check the FAQ
+            </Link>
+            <Link
+                href="/terms"
+                className={clsx(styles.faqLink)}
             >
-                <Link
-                    href="/faqs"
-                    className={clsx(styles.faqLink)}
-                >
-                    Don’t forget to check the FAQ
-                </Link>
-                <Link
-                    href="/terms"
-                    className={clsx(styles.faqLink)}
-                >
-                    Don't forget to check the Terms and Services.
-                </Link>
-            </Flex>
+                Don’t forget to check the Terms and Services.
+            </Link>
         </Flex>
-    );
-};
+    </Flex>
+);
 
 export default CallToAction;

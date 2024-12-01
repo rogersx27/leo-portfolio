a50dc82bc99ad8ad6ac1cb32ac28ea7afb696405
Sebastian Preciado <preciadoproducer@gmail.com>
2024-11-30 19:00:08 -0500

    fix: restructure Footer component for improved layout and add links to Terms & Conditions and FAQs

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,5 +1,5 @@
-import { Flex, IconButton, SmartLink, Text } from "@/once-ui/components"
-import { person, social } from '@/app/resources'
+import { Flex, IconButton, SmartLink, Text } from "@/once-ui/components";
+import { person, social } from "@/app/resources";
 
 export const Footer = () => {
     const currentYear = new Date().getFullYear();
@@ -8,60 +8,61 @@ export const Footer = () => {
         <Flex
             as="footer"
             position="relative"
-            fillWidth padding="8"
-            zIndex={999} 
-            justifyContent="center">
-            <Flex
-                fillWidth maxWidth="m" paddingY="8" paddingX="16"
-                justifyContent="space-between" alignItems="center">
-                {/* Main Footer Text */}
-                <Flex direction="column" alignItems="flex-start">
-                    <Text
-                        variant="body-default-s"
-                        onBackground="neutral-strong">
-                        <Text onBackground="neutral-weak">
-                            © {currentYear} /
-                        </Text>
-                        <Text paddingX="4">
-                            {person.name}
-                        </Text>
-                        <Text onBackground="neutral-weak">
-                            / Build using <SmartLink style={{ marginLeft: '-0.125rem' }} href="https://once-ui.com/templates/magic-portfolio">Once UI</SmartLink>
-                        </Text>
+            fillWidth
+            padding="8"
+            zIndex={999}
+            justifyContent="center"
+            alignItems="center"
+            direction="column"
+        >
+            {/* Links to Terms and FAQs */}
+            <Flex direction="column" alignItems="center" gap="12" marginBottom="2">
+                <SmartLink href="/terms" style={{ textDecoration: "none" }}>
+                    <Text variant="body-default-s" onBackground="neutral-strong">
+                        Terms & Conditions
                     </Text>
-                    {/* Links to Terms and FAQs */}
-                    <Flex
-                        direction="column"
-                        alignItems="flex-start"
-                        gap="4"
-                        marginTop="8">
-                        <SmartLink href="/terms" style={{ textDecoration: 'none' }}>
-                            <Text variant="body-default-s" onBackground="neutral-strong">
-                                Terms & Conditions
-                            </Text>
-                        </SmartLink>
-                        <SmartLink href="/faqs" style={{ textDecoration: 'none' }}>
-                            <Text variant="body-default-s" onBackground="neutral-strong">
-                                FAQs
-                            </Text>
-                        </SmartLink>
-                    </Flex>
-                </Flex>
-                {/* Social Icons */}
-                <Flex gap="16">
-                    {social.map((item) => (
-                        item.link && (
-                            <IconButton
-                                key={item.name}
-                                href={item.link}
-                                icon={item.icon}
-                                tooltip={item.name}
-                                size="s"
-                                variant="ghost" />
-                        )
-                    ))}
-                </Flex>
+                </SmartLink>
+                <SmartLink href="/faqs" style={{ textDecoration: "none" }}>
+                    <Text variant="body-default-s" onBackground="neutral-strong">
+                        FAQs
+                    </Text>
+                </SmartLink>
             </Flex>
+
+            {/* Social Icons */}
+            <Flex gap="16" marginBottom="16">
+                {social.map((item) => (
+                    item.link && (
+                        <IconButton
+                            key={item.name}
+                            href={item.link}
+                            icon={item.icon}
+                            tooltip={item.name}
+                            size="s"
+                            variant="ghost"
+                        />
+                    )
+                ))}
+            </Flex>
+
+            {/* Footer Text */}
+            <Text
+                variant="body-default-s"
+                onBackground="neutral-strong"
+                textAlign="center"
+            >
+                <Text onBackground="neutral-weak">© {currentYear} /</Text>
+                <Text paddingX="4">{person.name}</Text>
+                <Text onBackground="neutral-weak">
+                    / Build using{" "}
+                    <SmartLink
+                        style={{ marginLeft: "-0.125rem" }}
+                        href="https://once-ui.com/templates/magic-portfolio"
+                    >
+                        Once UI
+                    </SmartLink>
+                </Text>
+            </Text>
         </Flex>
-    )
-}
+    );
+};
